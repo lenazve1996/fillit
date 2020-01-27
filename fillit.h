cd5e03f2fff11842a6dea3cb4ab1e4a8ceb19407
Allen Yajirobe <ayajirob@il-d2.21-school.ru>
2020-01-27 16:44:02 +0300

    .

---
+++ fillit.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayajirob <ayajirob@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/27 16:10:05 by ayajirob          #+#    #+#             */
+/*   Updated: 2020/01/27 16:43:07 by ayajirob         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FILLIT_H
+# define FILLIT_H
+
+# include "../libft/libft.h"
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+
+typedef struct s_tetr
+{
+    char            **tetra;
+    int             wid;
+    int             hei;
+    char            val;
+    struct  s_tetr  *next;
+}              t_tetr;
+
+s_tetr  *read_tetra(int fd);
+#endif
--- reader.c
+++ reader.c
@@ -1,31 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fillit.h                                           :+:      :+:    :+:   */
+/*   reader.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ayajirob <ayajirob@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/01/27 16:10:05 by ayajirob          #+#    #+#             */
-/*   Updated: 2020/01/27 16:39:14 by ayajirob         ###   ########.fr       */
+/*   Created: 2020/01/27 16:41:10 by ayajirob          #+#    #+#             */
+/*   Updated: 2020/01/27 16:43:10 by ayajirob         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FILLIT_H
-# define FILLIT_H
+#include "fillit.h"
 
-# include "../libft/libft.h"
-# include <fcntl.h>
-# include <sys/types.h>
-# include <sys/uio.h>
-
-typedef struct s_tetr
+s_tetr  *read_tetra(int fd)
 {
-    char            **tetra;
-    int             wid;
-    int             hei;
-    char            val;
-    struct  s_tetr  *next;
-}              t_tetr;
-
-s_tetr  *read_tetra(int fd);
-#endif
+    
+}
